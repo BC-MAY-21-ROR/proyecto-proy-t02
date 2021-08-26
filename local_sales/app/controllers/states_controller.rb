f6521159845a1854786c8faa99679354baec217b
Jairo <olan.mayo05@gmail.com>
2021-08-26 11:47:00 -0500

    add state controller and actions

---
+++ local_sales/app/controllers/states_controller.rb
@@ -0,0 +1,50 @@
+class StatesController < ApplicationController
+  before_action :set_state, only: %i[ show edit update ]
+
+  # GET /states
+  def index
+    @states = State.all
+  end
+
+  # GET /states/new
+  def new
+	  @state = State.new
+  end
+
+	# GET /states/1/edit
+  def edit
+  end
+
+  # POST /states
+  def create
+    @state = State.new(state_params)
+    if @state.save
+      flash[:success] = 'state was successfully created'
+      redirect_to states_path
+    else
+      flash[:error] = 'state was not created'
+    end
+  end
+
+	# PATCH/PUT /states/1
+  def update
+    if @state.update(state_params)
+      flash[:success] = 'state was successfully updated'
+      redirect_to states_path
+    else
+      flash[:error] = 'state was not updated'
+    end
+  end
+
+  private
+
+		# Use callbacks to share common setup or constraints between actions.
+	  def set_state
+	    @state = State.find(params[:id])
+	  end
+
+	  # Only allow a list of trusted parameters through.
+	  def state_params
+      params.require(:state).permit(:name)
+    end
+end
