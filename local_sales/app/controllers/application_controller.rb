def9da9fe09c98b3b7c52617647b8d934c533ed5
julio cabrera <juliocabrera820@gmail.com>
2021-09-01 20:26:40 -0500

    skip authorization in devise controller
    by using this change, all kind of user can access to sign in page

--- local_sales/app/controllers/application_controller.rb
+++ local_sales/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  load_and_authorize_resource
+  load_and_authorize_resource unless: :devise_controller?
   include Pagy::Backend
 
   rescue_from CanCan::AccessDenied, with: :access_denied
