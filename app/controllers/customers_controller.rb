8cdbe3fb6f6d5f897fc10864abfa20919440d450
julio cabrera <juliocabrera820@gmail.com>
2021-09-06 11:34:23 -0500

    skip before action to see all resources without a session
    devise helper is not necessary, because all customer pages must be available for everyone

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
+  skip_before_action :authenticate_user!
   skip_load_and_authorize_resource
+
   def index
     @categories = Category.limit(2)
     @featured_products = Product.all
