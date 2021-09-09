e1060e5d5788cf57f0669e6f159368df8a3a4ab5
Jairo <olan.mayo05@gmail.com>
2021-09-09 18:48:04 -0500

    add flash for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
   include Pagy::Frontend
+  
+  def build_alert_classes(alert_type)
+    classes = ' '
+    case alert_type.to_sym
+    when :alert, :danger, :error, :validation_errors
+      classes += 'error'
+    when :warning, :todo
+      classes += 'warning'
+    when :notice, :success
+      classes += 'success'
+    end
+  end
 end
